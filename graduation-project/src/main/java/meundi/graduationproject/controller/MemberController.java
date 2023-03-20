db340cf527b0905ef6cc5965dbace632a5255551
famo1245 <kandallee38@gmail.com>
2023-03-21 01:55:20 +0900

    repository 추가

--- graduation-project/src/main/java/meundi/graduationproject/controller/MemberController.java
+++ graduation-project/src/main/java/meundi/graduationproject/controller/MemberController.java
@@ -1,21 +1,30 @@
 package meundi.graduationproject.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import meundi.graduationproject.domain.Member;
 import meundi.graduationproject.domain.Tiers;
+import meundi.graduationproject.repository.MemberRepository;
 import meundi.graduationproject.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
+import java.util.Optional;
+
+@Slf4j
 @Controller
 public class MemberController {
     private final MemberService memberService;
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public MemberController(MemberService memberService) {
+    public MemberController(MemberService memberService, MemberRepository memberRepository) {
         this.memberService = memberService;
+        this.memberRepository = memberRepository;
     }
 
     @GetMapping("/members/new")
@@ -38,5 +47,17 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/members/info")
+    public String myInfo(Model model, HttpSession session) {
+        String userId = (String) session.getAttribute("userId");
+        Optional<Member> findMember = memberRepository.findById(userId);
+        if(findMember.isPresent()) {
+            Member myInfo = findMember.get();
+            model.addAttribute("myInfo", myInfo);
+        }
+
+        return "/members/myInfo";
     }
 }
