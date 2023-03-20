3e62614a101e80b0b1c149276b1f50957b0cd3b2
famo1245 <kandallee38@gmail.com>
2023-03-21 01:03:55 +0900

    slf4j 추가

--- graduation-project/src/main/java/meundi/graduationproject/controller/MemberController.java
+++ graduation-project/src/main/java/meundi/graduationproject/controller/MemberController.java
@@ -4,7 +4,6 @@ import meundi.graduationproject.domain.Member;
 import meundi.graduationproject.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
