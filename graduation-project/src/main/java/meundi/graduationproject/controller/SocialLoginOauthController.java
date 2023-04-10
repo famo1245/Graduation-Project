e2ca51c1e7d0ef029549375fb4a7148d90283b42
famo1245 <kandallee38@gmail.com>
2023-04-10 15:30:16 +0900

    Refactoring 용 구현 전이라 주석 처리

--- graduation-project/src/main/java/meundi/graduationproject/controller/SocialLoginOauthController.java
+++ graduation-project/src/main/java/meundi/graduationproject/controller/SocialLoginOauthController.java
@@ -1,23 +1,23 @@
-package meundi.graduationproject.controller;
-
-import lombok.RequiredArgsConstructor;
-import lombok.experimental.PackagePrivate;
-import lombok.extern.slf4j.Slf4j;
-import meundi.graduationproject.domain.login.SocialLoginType;
-import meundi.graduationproject.service.OauthService;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@CrossOrigin
-@RequiredArgsConstructor
-@RequestMapping(value = "/auth")
-@Slf4j
-public class SocialLoginOauthController {
-    private final OauthService oauthService;
-
-    @GetMapping(value = "/{socialLoginType}")
-    public void socialLoginTyoe(@PathVariable(name = "socialLoginType")SocialLoginType socialLoginType){
-        log.info(">>사용자로부터 SNS 로그인 요청을 받음 :: {} Social Login", socialLoginType);
-        oauthService.request(socialLoginType);
-    }
-}
+//package meundi.graduationproject.controller;
+//
+//import lombok.RequiredArgsConstructor;
+//import lombok.experimental.PackagePrivate;
+//import lombok.extern.slf4j.Slf4j;
+//import meundi.graduationproject.domain.login.SocialLoginType;
+//import meundi.graduationproject.service.OauthService;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+//@CrossOrigin
+//@RequiredArgsConstructor
+//@RequestMapping(value = "/auth")
+//@Slf4j
+//public class SocialLoginOauthController {
+//    private final OauthService oauthService;
+//
+//    @GetMapping(value = "/{socialLoginType}")
+//    public void socialLoginTyoe(@PathVariable(name = "socialLoginType")SocialLoginType socialLoginType){
+//        log.info(">>사용자로부터 SNS 로그인 요청을 받음 :: {} Social Login", socialLoginType);
+//        oauthService.request(socialLoginType);
+//    }
+//}
