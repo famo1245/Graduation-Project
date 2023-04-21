25a9155eebdf15aa6f30bb1435a31d701aaa5b1a
famo1245 <kandallee38@gmail.com>
2023-04-21 16:41:49 +0900

    소셜 로그인으로 묶기 위한 refactoring

--- graduation-project/src/main/java/meundi/graduationproject/service/KaKaoAPI.java
+++ graduation-project/src/main/java/meundi/graduationproject/service/KaKaoAPI.java
@@ -4,72 +4,102 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
 public class KaKaoAPI {
-    public String getAccessToken(String authorize_code) {
-        log.debug("method:getAccessToken");
-        String accessToken = "";
-        String refreshToken = "";
-        String reqURL = "https://kauth.kakao.com/oauth/token";
 
+    @Value("${api.kakao.url}")
+    private String KAKAO_SNS_BASE_URL;
+    @Value("${api.kakao.client.id}")
+    private String KAKAO_SNS_CLIENT_ID;
+    @Value("${api.kakao.callback.url}")
+    private String KAKAO_SNS_CALLBACK_URL;
+    @Value("${api.kakao.token.url}")
+    private String KAKAO_SNS_TOKEN_BASE_URL;
+    @Value("${api.kakao.user.url}")
+    private String KAKAO_SNS_USER_URL;
+    @Value("${api.kakao.logout.url}")
+    private String KAKAO_SNS_LOGOUT_URL;
+
+    public String getOauthRedirectUrl() {
+        log.info("kakao get redirect url");
+        Map<String, Object> params = new HashMap<>();
+        params.put("client_id", KAKAO_SNS_CLIENT_ID);
+        params.put("redirect_uri", KAKAO_SNS_CALLBACK_URL);
+        params.put("response_type", "code");
+
+        String parameterString = params.entrySet().stream()
+                .map(x -> x.getKey() + "=" + x.getValue())
+                .collect(Collectors.joining("&"));
+
+        return KAKAO_SNS_BASE_URL + "?" + parameterString;
+    }
+
+    public String getAccessToken(String code) {
         try {
-            URL url = new URL(reqURL);
+            URL url = new URL(KAKAO_SNS_TOKEN_BASE_URL);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 
             //    POST 요청을 위해 기본값이 false인 setDoOutput을 true로
             conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
             conn.setDoOutput(true);
 
+            Map<String, Object> params = new HashMap<>();
+            params.put("code", code);
+            params.put("client_id", KAKAO_SNS_CLIENT_ID);
+            params.put("redirect_uri", KAKAO_SNS_CALLBACK_URL);
+            params.put("grant_type", "authorization_code");
+
+            String parameterString = params.entrySet().stream()
+                    .map(x -> x.getKey() + "=" + x.getValue())
+                    .collect(Collectors.joining("&"));
+
             //    POST 요청에 필요로 요구하는 파라미터 스트림을 통해 전송
-            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
-            StringBuilder sb = new StringBuilder();
-            sb.append("grant_type=authorization_code");
-            sb.append("&client_id=92fbf81b06d378a41d55ee603e5b6bd0");
-            sb.append("&redirect_uri=http://localhost:8080/home/member");
-            sb.append("&code=" + authorize_code);
-            bw.write(sb.toString());
-            bw.flush();
+            BufferedOutputStream bous = new BufferedOutputStream(conn.getOutputStream());
+            bous.write(parameterString.getBytes());
+            bous.flush();
+            bous.close();
 
-            //결과 코드가 200이라면 성공
-            int responseCode = conn.getResponseCode();
-            log.info("responseCode={}", responseCode);
-
-            //요청을 통해 얻은 JSON타입의 Response 메세지 읽어오기
             BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            String line = "";
-            String result = "";
+
+            StringBuilder sb = new StringBuilder();
+            String line;
 
             while ((line = br.readLine()) != null) {
-                result += line;
+                sb.append(line);
             }
-            log.info("response body={}", result);
 
-            //    Gson 라이브러리에 포함된 클래스로 JSON파싱 객체 생성
-            JsonParser parser = new JsonParser();
-            JsonElement element = parser.parse(result);
+            if (conn.getResponseCode() == 200) {
+                String result = sb.toString();
+                log.info("response={}", result);
+                JsonParser parser = new JsonParser();
+                JsonElement element = parser.parse(result);
 
-            accessToken = element.getAsJsonObject().get("access_token").getAsString();
-            refreshToken = element.getAsJsonObject().get("refresh_token").getAsString();
+                String accessToken = element.getAsJsonObject().get("access_token").getAsString();
+                String refreshToken = element.getAsJsonObject().get("refresh_token").getAsString();
 
-            log.info("access token={}", accessToken);
-            log.info("refresh token={}", refreshToken);
+                log.info("access token={}", accessToken);
+                log.info("refresh token={}", refreshToken);
 
-            br.close();
-            bw.close();
+                return accessToken;
+            }
+            return "구글 로그인 요청 처리 실패";
         } catch (IOException e) {
             // TODO Auto-generated catch block
-            e.printStackTrace();
+            throw new IllegalArgumentException("알 수 없는 카카오 로그인Access Token 요청 URL 입니다 :: "
+                    + KAKAO_SNS_TOKEN_BASE_URL);
         }
-
-        return accessToken;
     }
 
     public HashMap<String, Object> getUserInfo (String access_Token) {
@@ -77,9 +107,8 @@ public class KaKaoAPI {
 
         //요청하는 클라이언트마다 가진 정보가 다를 수 있기에 HashMap타입으로 선언
         HashMap<String, Object> userInfo = new HashMap<>();
-        String reqURL = "https://kapi.kakao.com/v2/user/me";
         try {
-            URL url = new URL(reqURL);
+            URL url = new URL(KAKAO_SNS_USER_URL);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("POST");
 
@@ -91,12 +120,15 @@ public class KaKaoAPI {
 
             BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 
-            String line = "";
-            String result = "";
+            StringBuilder sb = new StringBuilder();
+            String line;
 
             while ((line = br.readLine()) != null) {
-                result += line;
+                sb.append(line);
             }
+
+            String result = sb.toString();
+
             log.info("response body={}", result);
 
             JsonParser parser = new JsonParser();
@@ -122,11 +154,10 @@ public class KaKaoAPI {
         return userInfo;
     }
 
-    public void kakaoLogout(String access_Token) {
+    public void logout(String access_Token) {
         log.debug("method: kakaoLogout");
-        String reqURL = "https://kapi.kakao.com/v1/user/logout";
         try {
-            URL url = new URL(reqURL);
+            URL url = new URL(KAKAO_SNS_LOGOUT_URL);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("POST");
             conn.setRequestProperty("Authorization", "Bearer " + access_Token);
@@ -136,12 +167,15 @@ public class KaKaoAPI {
 
             BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 
-            String result = "";
-            String line = "";
+            StringBuilder sb = new StringBuilder();
+            String line;
 
             while ((line = br.readLine()) != null) {
-                result += line;
+                sb.append(line);
             }
+
+            String result = sb.toString();
+            
             log.info("result={}", result);
         } catch (IOException e) {
             // TODO Auto-generated catch block
