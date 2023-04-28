b48295b33321c156f0b39a519729a9cbf783393a
famo1245 <kandallee38@gmail.com>
2023-04-28 15:31:58 +0900

    필요없는 import문 제거

--- graduation-project/src/main/java/meundi/graduationproject/controller/HomeController.java
+++ graduation-project/src/main/java/meundi/graduationproject/controller/HomeController.java
@@ -1,20 +1,9 @@
 package meundi.graduationproject.controller;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import meundi.graduationproject.repository.MemberRepository;
-import meundi.graduationproject.service.KaKaoAPI;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.util.HashMap;
 
 @Slf4j
 @Controller
