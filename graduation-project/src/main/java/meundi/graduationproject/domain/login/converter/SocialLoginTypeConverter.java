3f8acb7303cab23d40e20addd09855931542e0f8
famo1245 <kandallee38@gmail.com>
2023-04-28 15:32:34 +0900

    enum class로 로그인 타입 구분

--- graduation-project/src/main/java/meundi/graduationproject/domain/login/converter/SocialLoginTypeConverter.java
+++ graduation-project/src/main/java/meundi/graduationproject/domain/login/converter/SocialLoginTypeConverter.java
@@ -1,10 +1,11 @@
 package meundi.graduationproject.domain.login.converter;
 
 import meundi.graduationproject.domain.login.SocialLoginType;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 
-import java.lang.annotation.Annotation;
 
+@Configuration
 public class SocialLoginTypeConverter implements Converter<String, SocialLoginType> {
 
     @Override
