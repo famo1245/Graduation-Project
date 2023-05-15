9e6d66e76df32532aaf0423c1fb7fb26b8a77e6f
famo1245 <kandallee38@gmail.com>
2023-05-15 14:33:22 +0900

    회원가입 getmapping 수정

--- graduation-project/src/main/java/meundi/graduationproject/controller/MemberController.java
+++ graduation-project/src/main/java/meundi/graduationproject/controller/MemberController.java
@@ -9,10 +9,13 @@ import meundi.graduationproject.service.MemberService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Controller
@@ -21,9 +24,22 @@ import javax.servlet.http.HttpSession;
 public class MemberController {
     private final MemberService memberService;
 
-    @GetMapping("/new")
-    public String createForm() {
+    @ModelAttribute("districts")
+    public List<String> districtName() {
+        List<String> districts = new ArrayList<>();
+        districts.add("종로구");
+        return districts;
+    }
+
+    @GetMapping("/login")
+    public String login() {
         return "members/loginForm";
+    }
+
+    @GetMapping("/new")
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
     }
 
     @PostMapping("/new")
