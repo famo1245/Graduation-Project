e49c4ec86ce8e93ab8241a52f4efae74ad4e151b
LeeJaejun98 <jj980302@naver.com>
2023-08-08 21:20:26 +0900

    채팅 구현 1차

---
+++ graduation-project/src/main/java/meundi/graduationproject/domain/Chat.java
@@ -0,0 +1,41 @@
+package meundi.graduationproject.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Chat {
+
+    //채팅 고유번호
+    @Id
+    @GeneratedValue
+    @Column(name = "chatting_id")
+    private Long id;
+
+    //채팅 작성시간
+    private LocalDateTime chattingDateTime;
+
+    //채팅 내용
+    @NotBlank
+    private String chattingContents;
+
+    @ManyToOne
+    @JoinColumn(name = "culture_id")
+    private Culture culture;
+    @NotBlank
+    private String cultureTitle;
+    @OneToMany(mappedBy = "recomment")
+    private List<ChatRecomment> chatRecoments;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+}
