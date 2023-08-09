1501a7e6795fb5257f6847e8001e05a26bcf171e
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2023-08-09 18:03:36 +0900

    컨트롤러 테스트 추가

---
+++ graduation-project/src/test/java/meundi/graduationproject/controller/FriendControllerTest.java
@@ -0,0 +1,73 @@
+package meundi.graduationproject.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import javax.transaction.Transactional;
+import meundi.graduationproject.domain.friend.DTO.FriendInsertDTO;
+import meundi.graduationproject.domain.friend.DTO.FriendSearchDTO;
+import meundi.graduationproject.service.FriendService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
+@Transactional
+@AutoConfigureMockMvc
+class FriendControllerTest {
+    @Autowired
+    FriendService friendService;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper om;
+
+    @DisplayName("Friend 객체 리스트를")
+    @Nested
+    class test1 {
+        @DisplayName("내보낼 수 있다.")
+        @Test
+        void success() throws Exception {
+            //given
+            FriendInsertDTO dto = new FriendInsertDTO("testTitle", "testContents", 3,
+                "[M클래식 축제] 마포공연예술관광페스티벌 [타케자와 유토 피아노 리사이틀]", "20230808 12:03");
+            friendService.InsertFriend(dto);
+            //when, then
+            mockMvc.perform(get("/friend"))
+                .andExpect(status().isOk())
+                .andDo(print());
+        }
+        @DisplayName("검색을 통해 내보낼 수 있다.")
+        @Test
+        void successBySearch()throws Exception{
+            //given
+            FriendInsertDTO dto = new FriendInsertDTO("testTitle", "testContents", 3,
+                "[M클래식 축제] 마포공연예술관광페스티벌 [타케자와 유토 피아노 리사이틀]", "20230808 12:03");
+            friendService.InsertFriend(dto);
+            //when then
+            FriendSearchDTO friendSearchDTO = new FriendSearchDTO("testTitle");
+            String content = om.writeValueAsString(friendSearchDTO);
+            mockMvc.perform(post("/friend")
+                    .content(content)
+                    .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print());
+        }
+
+    }
+
+}
