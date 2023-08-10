67e11bd5d002e7429131c55331a6df7189668710
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2023-08-10 11:53:59 +0900

    FriendControllerTest 상세페이지 Test Case 추가

--- graduation-project/src/test/java/meundi/graduationproject/controller/FriendControllerTest.java
+++ graduation-project/src/test/java/meundi/graduationproject/controller/FriendControllerTest.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import javax.transaction.Transactional;
 import meundi.graduationproject.domain.friend.DTO.FriendInsertDTO;
 import meundi.graduationproject.domain.friend.DTO.FriendSearchDTO;
+import meundi.graduationproject.domain.friend.Friend;
 import meundi.graduationproject.service.FriendService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -153,7 +154,18 @@ class FriendControllerTest {
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.error").value("must not be blank"))
                 .andDo(print());
-
+        }
+        @DisplayName("확인할 수 있다.")
+        @Test
+        void successByDetail() throws Exception {
+            //given
+            FriendInsertDTO dto = new FriendInsertDTO("testTitle", "testContents", 3,
+                "[M클래식 축제] 마포공연예술관광페스티벌 [타케자와 유토 피아노 리사이틀]", "20230808 12:03");
+            Friend friend = friendService.InsertFriend(dto);
+            //when then
+            mockMvc.perform(get("/friend/{friend_id}",friend.getId()))
+                .andExpect(status().isOk())
+                .andDo(print());
         }
     }
 
