7bbe5d80f34d68e9d7bb4a064d8c4107551358bf
famo1245 <kandallee38@gmail.com>
2023-09-29 21:17:04 +0900

    Style: code formatting

--- graduation-project/src/main/web-project/src/routes/Home.js
+++ graduation-project/src/main/web-project/src/routes/Home.js
@@ -17,19 +17,16 @@ function Home(props) {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [isLogin, setIsLogin] = useState(false);
   const [star, setStar] = useState(3);
   const [nextStar, setNextStar] = useState(0);
 
   useEffect(() => {
     setLoading(true);
+    const userId = sessionStorage.getItem("userId") != null ? parseInt(sessionStorage.getItem("userId")) : -1;
+    setIsLogin(sessionStorage.getItem("userId") === null ? false : true);
     axios
-      .get(
-        `/api/home?userId=${
-          sessionStorage.getItem("userId") != null
-            ? parseInt(sessionStorage.getItem("userId"))
-            : -1
-        }`
-      )
+      .get(`/api/home?userId=${userId}`)
       .then((response) => {
         setData(response.data);
         setLoading(false);
@@ -61,21 +58,31 @@ function Home(props) {
   return (
     <div className={styles.home}>
       <div>
-        <h4>신규 문화 추천</h4>
+        <h4>{isLogin ? data.district : "신규 문화 추천"}</h4>
       </div>
       <div className={styles.home_upper}>
         <div className={styles.home_upper_content}>
-          {data.recentCultures.map((culture) => {
-            const url = `/munhwaRow/${culture.id}`;
-            return (
-              <div key={culture.id}>
-                <Link to={url}>
-                  <img src={culture.main_img} alt="상세페이지" />
-                </Link>
-              </div>
-            );
-          })}
-
+          {isLogin
+            ? data.guList.map((culture) => {
+                const url = `/munhwaRow/${culture.id}`;
+                return (
+                  <div key={culture.id}>
+                    <Link to={url}>
+                      <img src={culture.main_img} alt="상세페이지" />
+                    </Link>
+                  </div>
+                );
+              })
+            : data.recentCultures.map((culture) => {
+                const url = `/munhwaRow/${culture.id}`;
+                return (
+                  <div key={culture.id}>
+                    <Link to={url}>
+                      <img src={culture.main_img} alt="상세페이지" />
+                    </Link>
+                  </div>
+                );
+              })}
           <div className={styles.prev}>
             <i className={styles.prev_arrow}>◀</i>
           </div>
@@ -86,20 +93,31 @@ function Home(props) {
         </div>
       </div>
       <div>
-        <h4>곧 끝나는 문화</h4>
+        <h4>{isLogin ? data.category + " 문화 생활" : "곧 끝나는 문화 생활"}</h4>
       </div>
       <div className={styles.home_upper}>
         <div className={styles.home_upper_content}>
-          {data.recentCultures.map((culture) => {
-            const url = `/munhwaRow/${culture.id}`;
-            return (
-              <div key={culture.id}>
-                <Link to={url}>
-                  <img src={culture.main_img} alt="상세페이지" />
-                </Link>
-              </div>
-            );
-          })}
+          {isLogin
+            ? data.recommendList.map((culture) => {
+                const url = `/munhwaRow/${culture.id}`;
+                return (
+                  <div key={culture.id}>
+                    <Link to={url}>
+                      <img src={culture.main_img} alt="상세페이지" />
+                    </Link>
+                  </div>
+                );
+              })
+            : data.recentCultures.map((culture) => {
+                const url = `/munhwaRow/${culture.id}`;
+                return (
+                  <div key={culture.id}>
+                    <Link to={url}>
+                      <img src={culture.main_img} alt="상세페이지" />
+                    </Link>
+                  </div>
+                );
+              })}
 
           <div className={styles.prev}>
             <i className={styles.prev_arrow}>◀</i>
@@ -134,9 +152,7 @@ function Home(props) {
                 <div className={styles.container_star_ratings}>
                   <span className={styles.star_ratings_title}>평점:</span>
                   <div className={styles.star_ratings}>
-                    <div className={styles.star_ratings_fill}>
-                      {starCount()}
-                    </div>
+                    <div className={styles.star_ratings_fill}>{starCount()}</div>
                     <div className={styles.star_ratings_base}>{starBase()}</div>
                   </div>
                 </div>
@@ -144,18 +160,13 @@ function Home(props) {
                 <br />
                 <span>작성일자: 2023/06/06</span>
                 <br />
-                <span className={styles.user_short_text}>
-                  정말 멋있어요 강추!!!!합니다.
-                </span>
+                <span className={styles.user_short_text}>정말 멋있어요 강추!!!!합니다.</span>
               </div>
             </div>
             <div className={styles.content_both_inner}>
               <div>
                 <Link>
-                  <img
-                    className={styles.review_img}
-                    src={`img/beautifulroad.jpg`}
-                  />
+                  <img className={styles.review_img} src={`img/beautifulroad.jpg`} />
                 </Link>
               </div>
               <div className={styles.inner_content}>
@@ -166,9 +177,7 @@ function Home(props) {
                 <div className={styles.container_star_ratings}>
                   <span className={styles.star_ratings_title}>평점:</span>
                   <div className={styles.star_ratings}>
-                    <div className={styles.star_ratings_fill}>
-                      {starCount()}
-                    </div>
+                    <div className={styles.star_ratings_fill}>{starCount()}</div>
                     <div className={styles.star_ratings_base}>{starBase()}</div>
                   </div>
                 </div>
@@ -177,9 +186,8 @@ function Home(props) {
                 <span>작성일자: 2023/06/06</span>
                 <br />
                 <div className={styles.user_short_text}>
-                  정말 멋있어요 강추!!!!합니다. 너무좋아요 사랑해요 레미제라블
-                  너는 나의 하나뿐인 뮤지컬이야. 제발 후속작 이만개 내줘요
-                  제작자님!!
+                  정말 멋있어요 강추!!!!합니다. 너무좋아요 사랑해요 레미제라블 너는 나의 하나뿐인 뮤지컬이야. 제발
+                  후속작 이만개 내줘요 제작자님!!
                 </div>
               </div>
             </div>
@@ -197,9 +205,7 @@ function Home(props) {
                 <div className={styles.container_star_ratings}>
                   <span className={styles.star_ratings_title}>평점:</span>
                   <div className={styles.star_ratings}>
-                    <div className={styles.star_ratings_fill}>
-                      {starCount()}
-                    </div>
+                    <div className={styles.star_ratings_fill}>{starCount()}</div>
                     <div className={styles.star_ratings_base}>{starBase()}</div>
                   </div>
                 </div>
@@ -207,9 +213,7 @@ function Home(props) {
                 <br />
                 <span>작성일자: 2023/06/06</span>
                 <br />
-                <span className={styles.user_short_text}>
-                  정말 멋있어요 강추!!!!합니다.
-                </span>
+                <span className={styles.user_short_text}>정말 멋있어요 강추!!!!합니다.</span>
               </div>
             </div>
             <div className={styles.content_both_inner}>
@@ -226,9 +230,7 @@ function Home(props) {
                 <div className={styles.container_star_ratings}>
                   <span className={styles.star_ratings_title}>평점:</span>
                   <div className={styles.star_ratings}>
-                    <div className={styles.star_ratings_fill}>
-                      {starCount()}
-                    </div>
+                    <div className={styles.star_ratings_fill}>{starCount()}</div>
                     <div className={styles.star_ratings_base}>{starBase()}</div>
                   </div>
                 </div>
@@ -236,9 +238,7 @@ function Home(props) {
                 <br />
                 <span>작성일자: 2023/06/06</span>
                 <br />
-                <span className={styles.user_short_text}>
-                  정말 멋있어요 강추!!!!합니다.
-                </span>
+                <span className={styles.user_short_text}>정말 멋있어요 강추!!!!합니다.</span>
               </div>
             </div>
           </div>
@@ -254,10 +254,7 @@ function Home(props) {
             <div className={styles.content_both_inner}>
               <div>
                 <Link>
-                  <img
-                    className={styles.review_img}
-                    src={`img/whitehorse.jpg`}
-                  />
+                  <img className={styles.review_img} src={`img/whitehorse.jpg`} />
                 </Link>
               </div>
               <div className={styles.inner_content}>
@@ -326,10 +323,7 @@ function Home(props) {
             <div className={styles.content_both_inner}>
               <div>
                 <Link>
-                  <img
-                    className={styles.review_img}
-                    src={`img/waterandhouse.jpg`}
-                  />
+                  <img className={styles.review_img} src={`img/waterandhouse.jpg`} />
                 </Link>
               </div>
               <div className={styles.inner_content}>
