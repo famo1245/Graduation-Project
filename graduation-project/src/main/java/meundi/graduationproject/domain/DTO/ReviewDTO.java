8ea9b585ea8c50419f9225f0464c8129b966fa2d
famo1245 <kandallee38@gmail.com>
2023-10-02 00:43:13 +0900

    프론트 엔드로 넘길 review data dto 정리

--- graduation-project/src/main/java/meundi/graduationproject/domain/DTO/ReviewDTO.java
+++ graduation-project/src/main/java/meundi/graduationproject/domain/DTO/ReviewDTO.java
@@ -2,11 +2,16 @@ package meundi.graduationproject.domain.DTO;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
+import meundi.graduationproject.domain.Member;
+import meundi.graduationproject.domain.Review;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 
-@Getter @Setter
+@Getter @Setter @ToString
 public class ReviewDTO {
 
     private Long id;
@@ -18,5 +23,23 @@ public class ReviewDTO {
     private String reviewTitle;
     /*평점: 0~5*/
     private int reviewGrade;
+    private int jimCount;
+    private List<Long> jimMember;
+    private String nickname;
+    private String main_img;
 
+    public void setReviewDTO(Review review, String nickname, String main_img) {
+        this.id = review.getId();
+        this.reviewContents = review.getReviewContents();
+        this.reviewGrade = review.getReviewGrade();
+        this.reviewTitle = review.getReviewTitle();
+        this.reviewDateTime = review.getReviewDateTime();
+        this.nickname = nickname;
+        this.main_img = main_img;
+        this.jimCount = review.getJim();
+        this.jimMember = new ArrayList<>();
+        for (Member m : review.getJimMember()) {
+            jimMember.add(m.getId());
+        }
+    }
 }
